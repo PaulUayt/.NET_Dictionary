fbb18b899eb813bc33d81a91186196f301dc3bd4
PaulUayt <pauluayt@gmail.com>
2021-11-10 01:21:28 +0200

    Добавьте файлы проекта.

---
+++ Dictionary_1/Map.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections;
+
+namespace Dictionary_1
+{
+    class Map<TKey, TValue>: IEnumerable
+    {
+        private int size = 100;
+        private Item<TKey, TValue>[] Items;
+        public Map() 
+        {
+            Items = new Item<TKey, TValue>[100];
+        }
+        public void Add(Item<TKey, TValue> item)
+        {
+            var hash = GetHash(item.Key);
+
+            if(Items[hash] == null)
+            {
+                Items[hash] = item;
+            }
+            else
+            {
+                var placed = false;
+                for(var i = hash; i < size; i++)
+                {
+                    if (Items[i] == null)
+                    {
+                        Items[i] = item;
+                        placed = true;
+                        break;
+                    }
+
+                    if (Items[i].Key.Equals(item.Key))
+                    {
+                        return;
+                    }
+                }
+
+                if(!placed)
+                {
+                    for (var i = hash; i < size; i++)
+                    {
+                        if (Items[i].Key.Equals(item.Key))
+                        {
+                            return;
+                        }
+
+                        if (Items[i] == null)
+                        {
+                            Items[i] = item;
+                            placed = true;
+                            break;
+                        }
+                    }
+                }
+
+                if(!placed)
+                {
+                    throw new Exception("Dictionary is full");
+                }
+            }
+        }
+
+        public void Remove(TKey key)
+        {
+            var hash = GetHash(key);
+
+            if (Items[hash] == null)
+            {
+                return;
+            }
+
+            if (Items[hash].Key.Equals(key))
+            {
+                Items[hash] = null;
+            }
+            else
+            {
+                var placed = false;
+                for (var i = hash; i < size; i++)
+                {
+                    if (Items[i] == null)
+                    {
+                        return;
+                    }
+
+                    if (Items[i].Key.Equals(key))
+                    {
+                        Items[i] = null;
+                        return;
+                    }
+                }
+
+                if (!placed)
+                {
+                    for (var i = hash; i < size; i++)
+                    {
+                        if (Items[i] == null)
+                        {
+                            return;
+                        }
+
+                        if (Items[i].Key.Equals(key))
+                        {
+                            Items[i] = null;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        public TValue Search(TKey key)
+        {
+            var hash = GetHash(key);
+
+            if(Items[hash] == null)
+            {
+                return default(TValue);
+            }
+
+            if (Items[hash].Key.Equals(key))
+            {
+                return Items[hash].Value;
+            }
+            else
+            {
+                var placed = false;
+                for (var i = hash; i < size; i++)
+                {
+
+                    if (Items[i] == null)
+                    {
+                        return default(TValue);
+                    }
+
+                    if (Items[i].Key.Equals(key))
+                    {
+                        return Items[i].Value;
+                    }
+                }
+
+                if (!placed)
+                {
+                    for (var i = hash; i < size; i++)
+                    {
+                        if (Items[i] == null)
+                        {
+                            return default(TValue);
+                        }
+
+                        if (Items[i].Key.Equals(key))
+                        {
+                            return Items[i].Value;
+                        }
+                    }
+                }
+            }
+
+            return default(TValue);
+        }
+
+        private int GetHash(TKey key)
+        {
+            return key.GetHashCode() % size;
+        }
+        public IEnumerator GetEnumerator()
+        {
+            foreach(var item in Items)
+            {
+                if(item != null)
+                {
+                    yield return item;
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Dictionary_1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*var easyMap = new EasyMap<int, string>();
+            easyMap.Add(new Item<int, string>(1, "One"));
+            easyMap.Add(new Item<int, string>(2, "Two"));
+            easyMap.Add(new Item<int, string>(3, "Three"));
+            easyMap.Add(new Item<int, string>(4, "Four"));
+            easyMap.Add(new Item<int, string>(5, "Five")); 
+
+            foreach(var item in easyMap)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(easyMap.Search(7) ?? "Not find");
+            Console.WriteLine(easyMap.Search(3) ?? "Not find");
+
+            easyMap.Remove(3);
+            easyMap.Remove(1);
+
+            foreach (var item in easyMap)
+            {
+                Console.WriteLine(item);
+            }*/
+
+
+            var dict = new Map<int, string>();
+            dict.Add(new Item<int, string>(1, "One"));
+            dict.Add(new Item<int, string>(2, "Two"));
+            dict.Add(new Item<int, string>(3, "Three"));
+            dict.Add(new Item<int, string>(4, "Four"));
+            dict.Add(new Item<int, string>(5, "Five"));
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(dict.Search(7) ?? "Not find");
+            Console.WriteLine(dict.Search(3) ?? "Not find");
+
+            dict.Remove(3);
+            dict.Remove(1);
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
