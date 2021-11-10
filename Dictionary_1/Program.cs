46340ab8b495ecf57067c036aa96079628004e7b
PaulUayt <pauluayt@gmail.com>
2021-11-10 02:34:35 +0200

    New tests

--- Dictionary_1/Program.cs
+++ Dictionary_1/Program.cs
@@ -10,36 +10,15 @@ namespace Dictionary_1
     {
         static void Main(string[] args)
         {
-            /*var easyMap = new EasyMap<int, string>();
-            easyMap.Add(new Item<int, string>(1, "One"));
-            easyMap.Add(new Item<int, string>(2, "Two"));
-            easyMap.Add(new Item<int, string>(3, "Three"));
-            easyMap.Add(new Item<int, string>(4, "Four"));
-            easyMap.Add(new Item<int, string>(5, "Five")); 
-
-            foreach(var item in easyMap)
-            {
-                Console.WriteLine(item);
-            }
-
-            Console.WriteLine(easyMap.Search(7) ?? "Not find");
-            Console.WriteLine(easyMap.Search(3) ?? "Not find");
-
-            easyMap.Remove(3);
-            easyMap.Remove(1);
-
-            foreach (var item in easyMap)
-            {
-                Console.WriteLine(item);
-            }*/
-
-
             var dict = new Map<int, string>();
+
+            dict.Add(new Item<int, string>(1, "One"));
             dict.Add(new Item<int, string>(1, "One"));
             dict.Add(new Item<int, string>(2, "Two"));
             dict.Add(new Item<int, string>(3, "Three"));
             dict.Add(new Item<int, string>(4, "Four"));
             dict.Add(new Item<int, string>(5, "Five"));
+            dict.Add(new Item<int, string>(101, "One hundred one"));
 
             foreach (var item in dict)
             {
@@ -49,8 +28,10 @@ namespace Dictionary_1
             Console.WriteLine(dict.Search(7) ?? "Not find");
             Console.WriteLine(dict.Search(3) ?? "Not find");
 
+            dict.Remove(7);
             dict.Remove(3);
             dict.Remove(1);
+            dict.Remove(101);
 
             foreach (var item in dict)
             {
